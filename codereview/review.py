9e76fc0d6d59e738a5cc5e32af44c648e06fa60a
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-16 14:49:05 +0100

    Add long listing of review item

--- codereview/review.py
+++ codereview/review.py
@@ -26,6 +26,9 @@ class Runner(object):
         for x, review in enumerate(self.reviews, start=1):
             review.print_short(x)
 
+    def show(self, index):
+        self.reviews[index - 1].show(index)
+
     def get_git_root(self):
         if os.path.isdir('.git'):
             return os.getcwd()
@@ -75,6 +78,7 @@ class Review(object):
         self.created = None
 
     def setup(self):
+        self.__dict__.update(self.data)
         self.created = dateutil.parser.parse(self.data['dates']['created'])
         self.merged = self.data['merged']
 
@@ -113,6 +117,35 @@ class Review(object):
 
         print(''.join(data))
 
+    def show(self, index):
+        self.print_short(index)
+        data = []
+
+        data.append('\n')
+        data.append(self.body.strip())
+        data.append('\n\n')
+
+        scores = []
+        for email, score in self.reviewers.items():
+            scores.append(
+                TERM.bold_cyan(email) +
+                TERM.bright_black(': ')
+            )
+            if score >= 1:
+                fun = TERM.bold_green
+                score = '+{0}'.format(score)
+            elif score == 0:
+                fun = TERM.bold_white
+            else:
+                fun = TERM.bold_red
+
+            scores.append(fun(str(score)))
+            scores.append('\n')
+
+        data.append(''.join(scores))
+
+        print(''.join(data))
+
 
 def setup_arguments():
     parser = argparse.ArgumentParser('codereview')
@@ -122,10 +155,20 @@ def setup_arguments():
         'new',
         help="Create a new review"
     )
+
     subparsers.add_parser(
         'list',
         help="List reviews"
     )
+
+    show = subparsers.add_parser(
+        'show',
+        help="Detailed view of a review"
+    )
+    show.add_argument(
+        'id', help="Review ID", default=1, nargs="?", type=int
+    )
+
     return parser
 
 
@@ -139,6 +182,10 @@ def main():
     if not ns.command or ns.command == 'list':
         runner.load_reviews()
         runner.list()
+
+    if ns.command == 'show':
+        runner.load_reviews()
+        runner.show(ns.id)
 
     if ns.command == 'new':
         print('Creating new review')
